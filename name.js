ac8404b49944edd212ecdcf819140b06f31cc947
Jo_wanja <jojodiva2@gmail.com>
2022-03-05 20:14:36 +0300

    initializng js script

---
+++ name.js
@@ -0,0 +1,7 @@
+
+function fetchdate(){
+document.getElementById("date").value
+console.log(typeof(document.getElementById("date").value))
+
+return console.log(document.getElementById("date").value)
+}
